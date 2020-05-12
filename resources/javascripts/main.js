e31b9c10a4587cc721af9c0d51264226e3675881
bhavesh732 <bhaveshkhivesra@gmail.com>
2020-05-12 21:57:19 +0530

    first commit

---
+++ resources/javascripts/main.js
@@ -0,0 +1,33 @@
+var prevScrollpos = window.pageYOffset;
+
+function hidefunction() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-100px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+var links = document.getElementsByClassName('butt');
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+        var active = document.getElementsByClassName('nav-a');
+        active[0].className = active[0].className.replace(" nav-a", "");
+        this.className += " nav-a";
+    });
+}
+
+const navbutton = document.querySelector('.navdisplay');
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('navhide');
+});
+
+var mq = window.matchMedia("(max-width: 766px)");
+if (mq.matches) {
+    function navhide() {
+        navbutton.click();
+    }
+}
